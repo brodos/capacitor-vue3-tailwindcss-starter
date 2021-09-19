009fc6322c3f730dc0b66f3d209bae63840813cf
Lucian B <brodos@outlook.com>
2021-09-19 04:32:37 -0500

    added tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,9 @@ module.exports = {
     extend: {},
   },
   plugins: [
-  	require('@tailwindcss/forms'),
   	require('@tailwindcss/typography'),
+  	require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
   ],
 }
